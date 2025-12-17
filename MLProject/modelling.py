6461ceef53ba803133ee93ef0789a28cd9153e99
Lens <hafizalasfawadhiah4@gmail.com>
2025-12-18 02:25:54 +0800

    fix code all

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -8,9 +8,7 @@ from sklearn.metrics import accuracy_score
 
 
 # Set MLFlow
-if os.getenv("GITHUB_ACTIONS") == "true":
-    mlflow.set_experiment("Pima-Diabetes-Basic")
-else:
+if os.getenv("GITHUB_ACTIONS") != "true":
     mlflow.set_tracking_uri("http://127.0.0.1:5000")
     mlflow.set_experiment("Pima-Diabetes-Basic")
 
